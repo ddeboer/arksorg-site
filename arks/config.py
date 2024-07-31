a7748d8d20c8041736d5856cf086397d6797f1e8
datadave <605409+datadavev@users.noreply.github.com>
2024-07-31 13:25:57 -0400

    Inherit from rslv config

--- arks/config.py
+++ arks/config.py
@@ -6,13 +6,14 @@ import os.path
 import typing
 
 import pydantic_settings
+import rslv.config
 
 ENV_PREFIX = "arks_"
 BASE_FOLDER = os.path.dirname(os.path.abspath(__file__))
 SETTINGS_FILE_KEY = f"{ENV_PREFIX.upper()}SETTINGS"
 
 
-class Settings(pydantic_settings.BaseSettings):
+class Settings(rslv.config.Settings):
     # env_prefix provides the environment variable prefix
     # for overriding these settings with env vars.
     # e.g. ARKS_PORT=11000
@@ -21,7 +22,7 @@ class Settings(pydantic_settings.BaseSettings):
     devhost: str = "localhost"
     # Port for the development server
     devport: int = 8000
-    # An sqlalchemy database connection string for the registry configuration
+    # Sqlalchemy database connection string for the registry configuration
     db_connection_string: str = f"sqlite:///{BASE_FOLDER}/data/registry.sqlite"
     # Label for the type of environment we are running under, e.g. "development", "production"
     environment: str = "production"
@@ -39,7 +40,14 @@ class Settings(pydantic_settings.BaseSettings):
     template_dir: str = os.path.join(BASE_FOLDER, "templates")
     # The public naan and shoulder source URL
     naans_source: str = "https://cdluc3.github.io/naan_reg_public/naan_records.json"
-
+    # Pattern to match this service URL endpoint, and if requests
+    # match then trim the service url from the PID
+    # For not uncommon situations where pid = "https://n2t.net/ark:/12345/foo"
+    service_pattern: typing.Optional[str] = None
+    # If pid value matches the definition value, then assume introspection.
+    # Note that this should be set False on services offering one-to-one matching of
+    # definitions to PIDs. For N2T and arks.org this sould be true to match legacy behavior.
+    auto_introspection: bool = True
 
 @functools.lru_cache
 def get_settings(env_file=None):
